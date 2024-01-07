a3b815f054e4e9d99758969aa969df3232d8e501
averpovskii <averpovskii@phoenixit.ru>
2024-01-07 19:45:29 +0300

    Sprint-3: create Counter app

--- Counter/Constants.swift
+++ Counter/Constants.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+enum Constants {
+    enum Action: String {
+        case plus = "+1"
+        case minus = "-1"
+    }
+    enum TimeFormat: String {
+        case fullDate = "YY, MMM d, HH:mm:ss"
+    }
+    enum StringsConstants: String {
+        case startHistoryLabel = "История изменений:\n"
+    }
+}
--- Counter/ViewController+Extensions.swift
+++ Counter/ViewController+Extensions.swift
@@ -5,4 +5,45 @@
 //  Created by Александр Верповский on 07.01.2024.
 //
 
-import Foundation
+import UIKit
+
+extension ViewController {
+    
+    func setupUI() {
+        setUpHistoryTV()
+        setUpCountLabel()
+        setupButtonOutlet(button: trashButtonOutlet, imageName: "trash")
+        setupButtonOutlet(button: plusButtonOutlet, imageName: "plus")
+        setupButtonOutlet(button: minusButtonOutlet, imageName: "minus")
+    }
+    
+    private func setupButtonOutlet(button: UIButton, imageName: String) {
+        button.setTitle("", for: .normal)
+        button.setImage(UIImage(systemName: imageName), for: .normal)
+        button.tintColor = .black
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.systemBlue.cgColor
+        button.layer.cornerRadius = button.bounds.height / 2
+        switch button {
+        case plusButtonOutlet:
+            button.backgroundColor = .systemRed
+        case minusButtonOutlet:
+            button.backgroundColor = .systemBlue
+        default:
+            break
+        }
+    }
+    
+    private func setUpHistoryTV() {
+        historyTV.text = StringsConstants.startHistoryLabel.rawValue
+        historyTV.isEditable = false
+        historyTV.textAlignment = .center
+        historyTV.font = UIFont.systemFont(ofSize: 18)
+    }
+    
+    private func setUpCountLabel() {
+        countLabel.font = UIFont.systemFont(ofSize: 20)
+        countLabel.text = "0"
+        countLabel.textAlignment = .center
+    }
+}
